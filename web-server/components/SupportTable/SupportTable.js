53fbaa185259ce40f82b6ab1e1849453b85ae33d
Dillon de Voor <dillon@crocodillon.com>
2018-03-24 23:43:24 +0100

    Improve styling browser support table a little bit

--- web-server/components/SupportTable/SupportTable.js
+++ web-server/components/SupportTable/SupportTable.js
@@ -7,26 +7,34 @@ import styles from './SupportTable.css'
 
 const SupportTable = props => {
   const { browsers } = props.property
+  const showAll = false
 
   return (
     <div className={styles.SupportTable}>
       {['ie', 'edge', 'firefox', 'chrome', 'safari', 'opera'].map(browser => (
         <div key={browser} className={styles.browser}>
-          <h2>{browser}</h2>
+          <h2 className={styles.browserName}>{browser}</h2>
           <ol className={styles.versions}>
             {Object.keys(usage.data[browser])
               .sort((a, b) => parseFloat(a) - parseFloat(b))
               .map(version => {
-                const support = get(browsers, `${browser}.${version}`, {})
-                const height = 10 * usage.data[browser][version]
+                const support = get(browsers, `${browser}.${version}`)
+                const height = showAll ? 20 : 10 * usage.data[browser][version]
+                const fontSize = height <= 20 ? 10 : null
+                const lineHeight = `${height}px`
 
                 const className = cn(styles.version, {
-                  [styles.supported]: support.in === true,
-                  [styles.unsupported]: support.in === false,
+                  [styles.hidden]: height < 10,
+                  [styles.supported]: support && support.in,
+                  [styles.unsupported]: support && !support.in,
                 })
 
                 return (
-                  <li key={version} className={className} style={{ height }}>
+                  <li
+                    key={version}
+                    className={className}
+                    style={{ height, fontSize, lineHeight }}
+                  >
                     {version}
                   </li>
                 )
