1c5a37a2502ce96b0456362f3a67d1d6a2a0b568
Dillon de Voor <dillon@crocodillon.com>
2018-04-02 11:45:48 +0200

    Move static head elements from page to document

--- web-server/components/Page/Page.js
+++ web-server/components/Page/Page.js
@@ -13,11 +13,6 @@ const Page = props => {
         <title>
           {title ? `${title} – browsersupport.io` : 'browsersupport.io'}
         </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css"
-        />
       </Head>
       <main className={styles.main}>{children}</main>
       <Footer />
--- web-server/pages/_document.js
+++ web-server/pages/_document.js
@@ -7,10 +7,18 @@ class CustomDocument extends Document {
     return (
       <html>
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <link
             rel="shortcut icon"
             type="image/png"
             href="/static/favicon.png"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css"
           />
           <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
